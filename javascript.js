4017c346e2d29b20fb2a68ea8dd9dc293519befa
Aiden Gaul <100671205+aidengaul@users.noreply.github.com>
2023-05-31 16:04:35 -0400

    Fixes double borders on grid box elements

--- javascript.js
+++ javascript.js
@@ -9,9 +9,19 @@ function initGrid() {
         const row = document.createElement('div');
         row.classList.add('row');
 
+        if (i === 0)
+            row.id = 'first-row';
+        if (i === 15) 
+            row.id = 'last-row';
+
         for (let j = 0; j < numBoxes; j++) {
+            // create the new grid box div
             const box = document.createElement('div');
             box.classList.add('grid-box');
+
+            if (j === 15)
+                box.classList.add('last-box');
+
             row.appendChild(box);
         }
         
