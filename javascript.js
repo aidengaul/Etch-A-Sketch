be58e577804e5677f46e01ddd5d9aa47a965c7a9
Aiden Gaul <100671205+aidengaul@users.noreply.github.com>
2023-05-31 16:39:33 -0400

    Restructures grid, grid boxes now able to scale to container size

--- javascript.js
+++ javascript.js
@@ -1,30 +1,29 @@
-const numBoxes = 16; // this is for both x and y
+const defaultBoxes = 16;
+let numBoxes = defaultBoxes; 
 
 function initGrid() {
     const container = document.getElementById('grid-container');
 
     for (let i = 0; i < numBoxes; i++) {
         // create the new row div
-        const row = document.createElement('div');
-        row.classList.add('row');
+        const column = document.createElement('div');
+        column.classList.add('column');
 
-        if (i === 0)
-            row.id = 'first-row';
-        if (i === 15) 
-            row.id = 'last-row';
+        if (i === numBoxes - 1)
+            column.classList.add('last-column');
 
         for (let j = 0; j < numBoxes; j++) {
             // create the new grid box div
             const box = document.createElement('div');
             box.classList.add('grid-box');
 
-            if (j === 15)
+            if (j === numBoxes - 1)
                 box.classList.add('last-box');
 
-            row.appendChild(box);
+            column.appendChild(box);
         }
         
-        container.appendChild(row);
+        container.appendChild(column);
     }
 
     const gridBoxes = document.querySelectorAll('.grid-box');
